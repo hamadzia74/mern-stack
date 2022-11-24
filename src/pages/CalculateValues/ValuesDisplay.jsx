1e57f379e6db5a3159c194eab84581076cc36feb
Hammad Zia <hammad@xeventechnologies.com>
2022-11-24 14:50:39 +0500

    Commit Changes

---
+++ src/pages/CalculateValues/ValuesDisplay.jsx
@@ -0,0 +1,11 @@
+// import { useState } from "react";
+
+// function ValuesDisplay() {
+
+//   return (
+//     <div className="values-display">
+//       <div className="values-comp">{num}</div>
+//     </div>
+//   );
+// }
+// export default ValuesDisplay;
