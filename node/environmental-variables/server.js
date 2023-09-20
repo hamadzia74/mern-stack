003f75e3ac5438d20b876f80d4d55b7aa1a19ba7
Hamad Zia <77169231+hamadzia74@users.noreply.github.com>
2023-09-20 22:38:45 +0500

    Add files via upload

--- node/environmental-variables/server.js
+++ node/environmental-variables/server.js
@@ -1,1 +1,17 @@
+const dotenv = require('dotenv');
+//Read our variables from the file & save them into node.js environment variables
+dotenv.config({path: './config.env'})
 
+const app = require('./app');
+
+
+//Environment Variables
+// console.log(app.get('env'));
+// Start the Server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`App running on Server ${port}...`)
+}
+)
+
+ 
